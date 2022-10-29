316c859b8a2d242513a7988bdfde934ecda67fff
Dmitrii Khaimin <khaimindm@gmail.com>
2022-10-29 18:16:24 +0400

    29.10.2022

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -1,16 +1,25 @@
 package com.epam.izh.rd.online.autcion.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class JdbcTemplateConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
 
     @Bean
     public JdbcTemplate jdbcTemplate() {
+        //Connection connection = DriverManager.
         return null;
     }
 
