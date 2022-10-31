171ac7fa7bfe7526cd1a40a0450972a9c7770af8
Dmitrii Khaimin <khaimindm@gmail.com>
2022-10-31 18:15:14 +0400

    31.10.22

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -1,6 +1,7 @@
 package com.epam.izh.rd.online.autcion.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,17 +15,30 @@ import java.sql.DriverManager;
 @PropertySource("classpath:application.properties")
 public class JdbcTemplateConfiguration {
 
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
     @Value("${spring.datasource.url}")
     private String url;
 
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        //Connection connection = DriverManager.
-        return null;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
     @Bean
     public DataSource dataSource() {
-        return null;
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
     }
 }
